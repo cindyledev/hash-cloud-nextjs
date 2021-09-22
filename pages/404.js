2cbc01b006a773a87a98002922bbc08be7ad79bf
Cindy Le <cindyledev@gmail.com>
2021-09-22 14:16:28 -0400

    updated 404 page to tailwind

--- pages/404.js
+++ pages/404.js
@@ -5,10 +5,41 @@ import Layout from '@components/Layout';
 export default function NotFoundPage() {
   return (
     <Layout title="Page Not Found">
-      <div>
-        <h1>404</h1>
-        <h4>Sorry, there is nothing here</h4>
-        <Link href="/">Go Back Home</Link>
+      <div className="min-h-screen pt-16 pb-12 flex flex-col bg-white">
+        <main className="flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex-shrink-0 flex justify-center">
+            <Link href="/">
+              <a className="inline-flex">
+                <span className="sr-only">Workflow</span>
+                <img
+                  className="h-12 w-auto"
+                  src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
+                  alt=""
+                />
+              </a>
+            </Link>
+          </div>
+          <div className="py-16">
+            <div className="text-center">
+              <p className="text-sm font-semibold text-indigo-600 uppercase tracking-wide">
+                404 error
+              </p>
+              <h1 className="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+                Page not found.
+              </h1>
+              <p className="mt-2 text-base text-gray-500">
+                Sorry, we couldn’t find the page you’re looking for.
+              </p>
+              <div className="mt-6">
+                <Link href="/">
+                  <a className="text-base font-medium text-indigo-600 hover:text-indigo-500">
+                    Go back home<span aria-hidden="true"> &rarr;</span>
+                  </a>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </main>
       </div>
     </Layout>
   );
