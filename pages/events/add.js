e10ae65e048c2af8e43463c378d25d9f908c30bd
Cindy Le <cindyledev@gmail.com>
2021-09-22 13:34:01 -0400

    updated Add Event to tailwind

--- pages/events/add.js
+++ pages/events/add.js
@@ -53,86 +53,162 @@ export default function AddEventsPage() {
 
   return (
     <Layout title="Add New Event">
-      <Link href="/events">Go Back</Link>
-      <h1>Add Event</h1>
-      <ToastContainer />
+      <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <ToastContainer />
 
-      <form onSubmit={handleSubmit}>
-        <div>
-          <div>
-            <label htmlFor="name">Event Name</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={values.name}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="performers">Performers</label>
-            <input
-              type="text"
-              id="performers"
-              name="performers"
-              value={values.performers}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="venue">Venue</label>
-            <input
-              type="text"
-              id="venue"
-              name="venue"
-              value={values.venue}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="address">Address</label>
-            <input
-              type="text"
-              id="address"
-              name="address"
-              value={values.address}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="date">Date</label>
-            <input
-              type="date"
-              id="date"
-              name="date"
-              value={values.date}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="time">Time</label>
-            <input
-              type="text"
-              id="time"
-              name="time"
-              value={values.time}
-              onChange={handleInputChange}
-            />
-          </div>
-        </div>
+        <form id="addEvent" onSubmit={handleSubmit} className="space-y-8 divide-y divide-gray-200">
+          <div className="space-y-8 divide-y divide-gray-200">
+            <div className="pt-8">
+              <div>
+                <h3 className="text-lg leading-6 font-medium text-gray-900">Add Event</h3>
+                <p className="mt-1 text-sm text-gray-500">Provide the event details.</p>
+              </div>
+              <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
+                <div className="sm:col-span-3">
+                  <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+                    Event Name
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      autoComplete="name"
+                      value={values.name}
+                      onChange={handleInputChange}
+                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
+                </div>
 
-        <div>
-          <label htmlFor="description">Event Description</label>
-          <textarea
-            type="text"
-            id="description"
-            name="description"
-            value={values.description}
-            onChange={handleInputChange}
-          ></textarea>
-        </div>
-        <input type="submit" value="Add Event" />
-      </form>
+                <div className="sm:col-span-3">
+                  <label htmlFor="performers" className="block text-sm font-medium text-gray-700">
+                    Performers
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      name="performers"
+                      id="performers"
+                      autoComplete="performers"
+                      value={values.performers}
+                      onChange={handleInputChange}
+                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-4">
+                  <label htmlFor="venue" className="block text-sm font-medium text-gray-700">
+                    Venue
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="venue"
+                      name="venue"
+                      type="text"
+                      autoComplete="venue"
+                      value={values.venue}
+                      onChange={handleInputChange}
+                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-6">
+                  <label htmlFor="address" className="block text-sm font-medium text-gray-700">
+                    Address
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      name="address"
+                      id="address"
+                      autoComplete="address"
+                      value={values.address}
+                      onChange={handleInputChange}
+                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-3">
+                  <label htmlFor="date" className="block text-sm font-medium text-gray-700">
+                    Date
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="date"
+                      id="date"
+                      name="date"
+                      autoComplete="date"
+                      value={values.date}
+                      onChange={handleInputChange}
+                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-2">
+                  <label htmlFor="time" className="block text-sm font-medium text-gray-700">
+                    Time
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      name="time"
+                      id="time"
+                      value={values.time}
+                      onChange={handleInputChange}
+                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-6">
+                  <label htmlFor="description" className="block text-sm font-medium text-gray-700">
+                    Description
+                  </label>
+                  <div className="mt-1">
+                    <textarea
+                      type="text"
+                      id="description"
+                      name="description"
+                      value={values.description}
+                      onChange={handleInputChange}
+                      rows={3}
+                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border border-gray-300 rounded-md"
+                      defaultValue={''}
+                    />
+                  </div>
+                  <p className="mt-2 text-sm text-gray-500">
+                    Write a few sentences about the event.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="pt-5 flex justify-between">
+            <div>
+              <button
+                type="button"
+                className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                <Link href="/events">Go Back</Link>
+              </button>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Add Event
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </Layout>
   );
 }
