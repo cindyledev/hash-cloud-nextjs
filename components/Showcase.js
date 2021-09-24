3e0c67a05e5bfb0e1a70412d8cc5be6bc2855ba8
Cindy Le <cindyledev@gmail.com>
2021-09-23 20:25:46 -0400

    updated showcase to tailwind

--- components/Showcase.js
+++ components/Showcase.js
@@ -1,8 +1,15 @@
 export default function Showcase() {
   return (
-    <div>
-      <h1>Welcome to the Party!</h1>
-      <h2>Find the hottest DJ events</h2>
-    </div>
+    <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
+      <div className="text-center">
+        <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+          <span className="block xl:inline">Welcome to the</span>{' '}
+          <span className="block text-indigo-600 xl:inline">Party!</span>
+        </h1>
+        <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+          Find the hottest DJ events
+        </p>
+      </div>
+    </main>
   );
 }
