05b1b46c13e63255dec651f19762f419e4445184
Cindy Le <cindyledev@gmail.com>
2021-09-28 13:01:13 -0400

    updated search result to tailwind

--- pages/events/search.js
+++ pages/events/search.js
@@ -12,50 +12,70 @@ export default function SearchPage({ events }) {
 
   return (
     <Layout title="Search Results">
-      <Link href="/events">Go Back</Link>
-      <h1>Search Results for {router.query.term}</h1>
-      <div>
-        <h2 className="text-gray-500 text-xs font-medium uppercase tracking-wide">Events</h2>
-        {events.length === 0 && <h3>No events to show</h3>}
-        <ul
-          role="list"
-          className="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4"
-        >
-          {events.map((evt) => (
-            <li key={evt.key} className="col-span-1 flex shadow-sm rounded-md">
-              <div>
-                <EventItem key={evt.key} evt={evt.name} />
-              </div>
-              <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
-                <div className="flex-1 px-4 py-2 text-sm truncate">
-                  <a
-                    href={`/events/${evt.slug}`}
-                    className="text-gray-900 font-medium hover:text-gray-600"
-                  >
-                    {evt.name}
-                  </a>
-                  <p className="text-gray-500">
-                    {new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
-                  </p>
-                </div>
-                <div className="flex-shrink-0 pr-2">
-                  <button
-                    type="button"
-                    className="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                  >
-                    <span className="sr-only">Open options</span>
+      <main>
+        <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+          <div className="relative max-w-7xl mx-auto">
+            <Link href="/events">
+              <a>{'<'} Go Back</a>
+            </Link>
+            <div className="text-center">
+              <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
+                Search Results for {router.query.term}
+              </h2>
+            </div>
 
-                    <a href={`/events/${evt.slug}`}>
-                      {' '}
-                      <DotsVerticalIcon className="w-5 h-5" aria-hidden="true" />
-                    </a>
-                  </button>
+            {events.length === 0 && <h3>No events to show</h3>}
+            <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+              {events.map((evt) => (
+                <div key={evt.key} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+                  <div className="flex-shrink-0">
+                    <img className="h-48 w-full object-cover" src={evt.imageUrl} alt="" />
+                  </div>
+                  <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                    <div className="flex-1">
+                      <p className="text-sm font-medium text-indigo-600">
+                        <Link href={`/events/${evt.slug}`}>
+                          <a className="hover:underline">{evt.performers}</a>
+                        </Link>
+                      </p>
+                      <Link href={`/events/${evt.slug}`}>
+                        <a className="block mt-2">
+                          <p className="text-xl font-semibold text-gray-900">{evt.name}</p>
+                          <p className="mt-3 text-base text-gray-500">{evt.description}</p>
+                        </a>
+                      </Link>
+                    </div>
+                    <div className="mt-6 flex items-center">
+                      <div className="flex-shrink-0">
+                        <Link href={`/events/${evt.slug}`}>
+                          <a>
+                            <span className="sr-only">{evt.venue}</span>
+                            <img className="h-10 w-10 rounded-full" src={evt.name} alt="" />
+                          </a>
+                        </Link>
+                      </div>
+                      <div className="ml-3">
+                        <p className="text-sm font-medium text-gray-900">
+                          <Link href={`/events/${evt.slug}`}>
+                            <a className="hover:underline">{evt.address}</a>
+                          </Link>
+                        </p>
+                        <div className="flex space-x-1 text-sm text-gray-500">
+                          <time dateTime={evt.date}>
+                            {new Date(evt.date).toLocaleDateString('en-US')}
+                          </time>
+                          <span aria-hidden="true">&middot;</span>
+                          <span>{evt.time}</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </main>
     </Layout>
   );
 }
